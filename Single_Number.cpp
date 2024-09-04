4026f9b35fa3e79e3c5ab86db9c516e6bd8903f4
PrajwalSinghBhadouria <151111886+PrajwalSinghBhadouria@users.noreply.github.com>
2024-09-04 21:25:09 +0530

    Add files via upload

---
+++ Single_Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int> mp;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        int p;
+        for(auto i: mp){
+            if(i.second == 1){
+                p = i.first;
+                break;
+            }
+        }
+        return p;
+
+    }
+};
